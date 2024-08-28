641bb0c52272dc145f27b945c9b1f7143a424180
Furkan Gülhan <fgulhan07@gmail.com>
2024-08-29 02:29:50 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ClipboardManager
+{
+    internal class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            bool newCopy = false;
+            List<string> copiedTexts = new List<string>();
+            
+            Clipboard.Clear();
+            Console.WriteLine("Clipboard Manager Started!");
+            
+            while (true)
+            {
+                // Ctrl + C is pressed
+                if ((Control.ModifierKeys & Keys.C) != 0 && (Control.ModifierKeys & Keys.Control) != 0)
+                {
+                    if (!newCopy)
+                    {
+                        newCopy = true;
+                    }
+                }
+                else
+                {
+                    // Ctrl + C is released
+                    if (newCopy)
+                    {
+                        if (Clipboard.ContainsText())
+                        {
+                            copiedTexts.Add(Clipboard.GetText());
+                        }
+                        else if (Clipboard.ContainsImage())
+                        {
+                            copiedTexts.Add("Image Copied!");
+                        }
+                        else if (Clipboard.ContainsAudio())
+                        {
+                            copiedTexts.Add("Audio Copied!");
+                        }
+
+                        Console.Clear();
+                        for (int i = 0; i < copiedTexts.Count; i++)
+                        {
+                            Console.WriteLine(i + ": " + copiedTexts[i]);
+                        }
+
+                        newCopy = false;
+                    }
+                }
+
+                Thread.Sleep(25);
+            }
+        }
+    }
+}
